8aad0e066b75bd47bb4e0b74038d9849a6f11e24
ayhamk94 <ayham.kteash1995@gmail.com>
2017-12-12 21:13:49 +0100

    fix: file extenions and linteted the project so all is working in the same style

--- src/widgets/weather/Weather.jsx
+++ src/widgets/weather/Weather.jsx
@@ -2,31 +2,30 @@ import React from 'react';
 import './weather.css';
 import MainInfo from './mainInfo';
 import Spinner from '../../components/Spinner';
+
 export default class Weather extends React.Component {
   constructor(props) {
     super(props);
-    this.state={data: {}}
-  };
+    this.state = { data: {} };
+  }
   componentDidMount() {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?q=berlin&units=metric&appid=e269b0432cb35577201f06837e2a5803`;
-    fetch(url).then( resp => resp.json()).then((weatherData)=> {
-      this.setState({ data:weatherData});
-      console.log(this.state.data);
-    })
+    const url = 'http://api.openweathermap.org/data/2.5/forecast?q=berlin&units=metric&appid=e269b0432cb35577201f06837e2a5803';
+    fetch(url).then(resp => resp.json()).then((weatherData) => {
+      this.setState({ data: weatherData });
+    });
   }
 
   render() {
     const { data } = this.state;
     return (
-      <div style={{height: '100%'}}>
-      {
+      <div style={{ height: '100%' }}>
+        {
         data.city ?
-        <MainInfo data={data}/>
+          <MainInfo data={data} />
         :
-        <Spinner/>
+          <Spinner />
       }
       </div>
-    )
+    );
   }
-
 }
