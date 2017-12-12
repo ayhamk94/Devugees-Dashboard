70aa95387e591eceeef561d42bf4b2157ffe3474
ayhamk94 <ayham.kteash1995@gmail.com>
2017-12-12 12:39:47 +0100

    added dark layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import './App.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Dashboard from './components/Dashboard';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
             <Dashboard/>
         </MuiThemeProvider>
       </div>
