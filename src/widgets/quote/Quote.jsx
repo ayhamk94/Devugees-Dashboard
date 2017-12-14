713c35dc00fc3db2404a9feed5589ad45528c53b
palomitaproy <palomita.c1@gmail.com>
2017-12-14 15:40:38 +0100

    add Quote

---
+++ src/widgets/quote/Quote.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+import Spinner from '../../components/Spinner';
+import './quote.css';
+
+export default class Quote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: [],
+      author: []
+    };
+
+  }
+
+  componentDidMount() {
+    var url = 'http://quotes.rest/qod.json';
+    var req = new Request(url);
+    fetch(req).then((response) => {
+      response.json().then(data => {
+    //    console.log('data: ', data)
+        let quotes = data.contents.quotes[0];
+        let quote = quotes.quote;
+        let author = quotes.author;
+        this.setState({quote, author});
+
+      }).catch((err) => {
+      //  console.log('Err: ', err);
+      });
+    })
+  }
+
+  render() {
+    const {quote, author} = this.state;
+
+    return (<div>
+      <div className="blockquote">
+        <p className="quotation-mark opening">&ldquo;</p>
+        <h3 className="quote">{quote}</h3>
+        <p className="quotation-mark closing">
+          &rdquo;</p>
+        <hr/>
+        <p className="author">{author}</p>
+      </div>
+    </div>)
+
+  }
+}
