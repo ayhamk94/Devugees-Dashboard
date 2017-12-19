9bf259a930ccb92709ccc8a7f563d44453c47f88
Thomas Kuhnert <spiel.hoelle@live.de>
2017-12-19 15:55:41 +0100

    show bitcoin rate

--- src/widgets/bitcoin/bitcoin.jsx
+++ src/widgets/bitcoin/bitcoin.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Spinner from '../../components/Spinner';
 
+const styles = {
+  rate: {
+    textAlign: 'justify',
+  },
+};
 export default class Bitcoin extends React.Component {
   constructor(props) {
     super(props);
@@ -8,22 +13,31 @@ export default class Bitcoin extends React.Component {
   }
   componentDidMount() {
     const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-    fetch(url).then(resp => resp.json()).then((bitocinData) => {
-      console.log("bitcoin data")
-      this.setState({ data: bitocinData });
-      console.log(this.state.data)
-      
+    fetch(url).then(resp => resp.json()).then((bitocoinData) => {
+      this.setState({ data: bitocoinData });
     });
   }
 
   render() {
     const { data } = this.state;
+    console.log(data)
     return (
       <div style={{ height: '100%' }}>
         {
-        data ?
+        data && data.bpi?
           <div>
-           { data.chartName}
+            <h1>Bitcoin Rate</h1>
+            <div style={styles.rate}>
+              Rate:
+              <div>
+                <span><strong>{data.bpi.EUR.rate.toString()}</strong><strong> €</strong></span>
+              </div>
+            <div>Updated: <br/> {data.time.updated}</div>
+              <br/>
+              <br/>
+              <small>{ data.disclaimer }</small>
+
+            </div>
           </div>
         :
           <Spinner />
