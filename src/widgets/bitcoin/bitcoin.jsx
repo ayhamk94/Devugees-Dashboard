c7277a772094bdf89aabe6e4929a3b5e72d850b0
Muhamad Asjad Baig <asgidbaig@gmail.com>
2017-12-20 11:37:28 +0100

    Added List and list items inside my widget using material UI

--- src/widgets/bitcoin/bitcoin.jsx
+++ src/widgets/bitcoin/bitcoin.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
 import Spinner from '../../components/Spinner';
+
 
 const styles = {
   rate: {
@@ -28,10 +32,12 @@ export default class Bitcoin extends React.Component {
           <div>
             <h1>Bitcoin Rate</h1>
             <div style={styles.rate}>
-              Rate:
-              <div>
-                <span><strong>{data.bpi.EUR.rate.toString()}</strong><strong> €</strong></span>
-              </div>
+            <List>
+            Rate:
+                <ListItem primaryText={data.bpi.USD.rate.toString()+" $"}  />
+                <ListItem primaryText={data.bpi.EUR.rate.toString()+" €"} />
+                <ListItem primaryText={data.bpi.GBP.rate.toString()+" £"} />
+              </List>
             <div>Updated: <br/> {data.time.updated}</div>
               <br/>
               <br/>
