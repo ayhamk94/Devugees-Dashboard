294cfa28b9469bea01b3d04c439a3a8a3c366307
Axel <axelfuh@gmail.com>
2017-12-20 10:03:13 +0100

    add localstorage state

--- src/App.js
+++ src/App.js
@@ -24,23 +24,45 @@ class App extends React.Component {
     this.state = {
       editMode: false,
       widgets: [],
+      mounted: []
     };
   }
   componentWillMount() {
     this.setState({ widgets: widgetsData });
+
+    if (!localStorage.getItem('mounted')) {
+      this.saveLocal(widgetsData)
+    } else {
+      const mounted = JSON.parse(localStorage.getItem('mounted'))
+      this.setState({ mounted })
+    }
+  }
+  handleLocalStorate(action){
+    this.saveLocal(action)
+  }
+  saveLocal(widgets){
+    const mounted = widgets.filter(w => w.mounted).map(w => w.id)
+    localStorage.setItem('mounted', JSON.stringify(mounted))
+    this.setState({ mounted })
+  }
+
+  componentDidMount(){
+    const { mounted } = this.state
+    console.log(mounted);
   }
 
   addRemoveWidgets(index) {
     const d = this.state.widgets;
     d[index].mounted = !d[index].mounted;
     this.setState({ widgets: d });
+    this.handleLocalStorate(d)
   }
 
 
   ToggleEditMode = () => { this.setState({ editMode: !this.state.editMode }); }
 
   render() {
-    const { widgets, editMode } = this.state;
+    const { widgets, editMode, drawerOpen, mounted } = this.state;
     return (
       <div className="App">
         <MuiThemeProvider muiTheme={appTheme}>
