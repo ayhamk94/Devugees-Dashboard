f2c8f8c409b2664e18bd8bc1dcf8eb3637b427ee
ns4000 <ns40002@gmail.com>
2017-12-21 11:34:11 +0100

    google map widget with no func so far

--- src/widgets/maps/Gmap.jsx
+++ src/widgets/maps/Gmap.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker,BicyclingLayer } from "react-google-maps";
-
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import SearchBox from "react-google-maps/lib/components/places/SearchBox";
 
 export default class Gmap extends Component{
 
@@ -10,8 +10,9 @@ export default class Gmap extends Component{
                 defaultZoom={15}
                 defaultCenter={{ lat: 52.523720, lng: 13.486234 }}
               >
-                {props.isMarkerShown && <Marker position={{ lat: 52.523720, lng: 13.486234 }} />}
+                {props.isMarkerShown && <Marker position={{ lat: 52.523720, lng: 13.486234 }} /> }
               </GoogleMap>
+
             ));
 
   return (
@@ -19,10 +20,10 @@ export default class Gmap extends Component{
       <MyMapComponent
         BicyclingLayer
         isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `85%` }} />}
+        containerElement={<div style={{ height: `360px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
       />
     </div>
   );
